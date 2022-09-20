e81b0fba3df68afaf80d092f41df5bfb5fba1b98
Veronica Leslie <v.leslie423@gmail.com>
2022-09-20 19:15:30 -0400

    week 9/19-9/23 java solutions

---
+++ SecondLargestOfThree.java
@@ -0,0 +1,50 @@
+// Problem Statement
+// Write a program that accepts sets of three numbers and prints the second-maximum number among the three.
+
+// Input
+
+	
+// First line contains the number of triples, N.
+	
+// The next N lines which follow each have three space separated integers.
+
+// Output
+// For each of the N triples, output one new line which contains the second-maximum integer among the three.
+
+// Constraints
+
+
+	
+// 1 ≤ N ≤ 6
+	
+// 1 ≤ every integer ≤ 10000
+	
+// The three integers in a single triplet are all distinct. That is, no two of them are equal.
+
+// Sample Input
+// 3
+// 1 2 3
+// 10 15 5
+// 100 999 500
+// Sample Output
+// 2
+// 10
+// 500
+
+import java.util.Arrays;  
+public class SecondLargestOfThree{
+public static int getSecondLargest(int[] A, int result){
+Arrays.sort(A);
+for (int i =0; i<2; i++){
+    if (A[i] == A[i+1]){
+        System.out.print("invalid inputs but  ");
+    }
+}
+return A[result-2];
+}
+public static void main(String args[]){
+int A[]={1,2,5};
+int B[]={44,44,99};
+System.out.println("Second Largest: "+getSecondLargest(A,3));
+System.out.println("Second Largest: "+getSecondLargest(B,3));
+}}
---
+++ SumDigitCharacters.java
@@ -0,0 +1,135 @@
+//// Problem statement
+//// Given an alphanumeric string made up of digits and lower case Latin characters only, find the sum of all the digit characters in the string.
+//
+//// Input
+//
+//
+//
+//// The first line of the input contains an integer T denoting the number of test cases. Then T test cases follow.
+//
+//// Each test case is described with a single line containing a string S, the alphanumeric string.
+//
+//// Output
+//
+//
+//
+//// For each test case, output a single line containing the sum of all the digit characters in that string.
+//
+//// Constraints
+//
+//
+//
+//// 1 ≤ T ≤ 1000
+//
+//// 1 ≤ |S| ≤ 1000, where |S| is the length of the string S.
+//
+//// Example
+//// Input:
+//// 1
+//// ab1231da
+//// Output:
+//// 7
+//
+//// Explanation
+//// The digits in this string are 1, 2, 3 and 1. Hence, the sum of all of them is 7.
+//
+//
+//import java.util.Scanner;
+//
+//class SumDigitCharacters {
+//
+//
+//    public SumDigitCharacters(String A) {
+//    }
+//
+//    private static int findSum(String A)
+//    {
+//
+//        char num;
+//        int a = 1;
+//        String temp = "";
+//
+//
+//        int sum = 0;
+//
+//
+//        for (int i = 0; i < A.length(); i++) {
+//            num = A.charAt(i);
+//
+//
+//            if (Character.isDigit(num)) {
+//                a = Character.getNumericValue(num);
+//                sum += a;
+//
+//            } else {
+//                temp += A.charAt(i);
+//                sum += Integer.parseInt(temp);
+//                temp = "";
+//            }
+//        }
+//
+//
+//        return sum;
+//    }
+//
+//
+////    public static void main(String[] args)
+////    {
+////
+////
+////        String str = "ab1231da";
+////
+////
+////        System.out.println(findSum(str));
+////    }
+////}
+
+import java.util.Scanner;
+
+public class SumDigitCharacters {
+
+    private static int sumOfDigits(String A) {
+
+        int sum = 0;
+        char num ;
+        int a;
+
+
+        for(int i=0; i<A.length(); i++) {
+
+           a = A.charAt(i);
+
+
+            if(Character.isDigit(num)){
+                a= Character. getNumericValue(num);
+
+                sum += a;
+            }
+        }
+
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+
+
+        String A = null;
+        int sum = 0;
+
+
+        Scanner scan =  new Scanner(System.in);
+
+
+        System.out.print("Enter String: ");
+        A = scan.next();
+        sum = sumOfDigits(A);
+
+        System.out.println("The sum of "
+                +" digits in the string "+A
+                +" = "+ sum);
+
+
+        scan.close();
+    }
+}
